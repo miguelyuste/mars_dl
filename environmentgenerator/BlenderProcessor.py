cccc4d56aa40b53b9d925be273a0b9db87f7dfc8
Miguel Yuste Fernández <miguel_y_fernandez@hotmail.com>
2020-11-06 14:54:48 +0100

    [EnvGen] added RGB and RGBD image generation

--- environmentgenerator/BlenderProcessor.py
+++ environmentgenerator/BlenderProcessor.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import os
 import bpy
 import mathutils
+from math import pi, tan
 
 
 def delete_scene_objects():
@@ -60,32 +61,48 @@ def fit_plane(vertices):
 
     return plane
 
-# todo: replace hardcoded values, rename class
 
 def reverse_mapping(array, from_min=0, from_max=1, to_min=0.1, to_max=100):
     slope = (to_max - to_min) / (from_max - from_min)
     return to_min + slope * (array - from_min)
 
 
-def get_depthmap():  # ACTUALLY, this is the DISTANCE map (distance of each pixel from the camera!)
+# distance map from camera to surfaces
+def get_distancemap_rgbmap():
     scene = bpy.data.scenes['Scene']
     tree = scene.node_tree
     # create output node
-    v = tree.nodes.new('CompositorNodeViewer')
-    v.use_alpha = False
-    depth = np.asarray(bpy.data.images["Viewer Node"].pixels)
-    depth = np.reshape(depth, (1080, 1920, 4))
-    depth = depth[:, :, 0]
-    depth = np.flipud(depth)
-    depth = reverse_mapping(depth)
+    # v = tree.nodes.new('CompositorNodeViewer')
+    # v.use_alpha = False
+
+    tree.nodes['Switch'].check = True
+    bpy.ops.render.render(write_still=False)
+    distance_map = np.asarray(bpy.data.images["Viewer Node"].pixels)
+    # todo: if camera resolution can be tweaked, this can be parametrised
+    distance_map = np.reshape(distance_map, (1080, 1920, 4))
+    distance_map = distance_map[:, :, 0]
+    distance_map = np.flipud(distance_map)
+    distance_map = reverse_mapping(distance_map)
+    # print(distancemap)
+
+    tree.nodes['Switch'].check = False
+    bpy.ops.render.render(write_still=False)
+    rgb_image = np.asarray(bpy.data.images["Viewer Node"].pixels)
+    # todo: if camera resolution can be tweaked, this can be parametrised
+    rgb_image = np.reshape(rgb_image, (1080, 1920, 4))
+    rgb_image = rgb_image[:, :, 0:3]
+    rgb_image = np.flipud(rgb_image)
+    rgb_image = reverse_mapping(rgb_image)
+    # print(rgbmap.min())
+    # print(rgbmap.max())
 
     # np.save(r"C:\Users\migue\Desktop/distance_from_camera.npy", depth)
-    return depth
+    return distance_map, rgb_image
 
 
-def distance_to_depth_conversion(dist_img, camera_fov=50.7):
-    img_width = dist_img.shape[1]
-    img_height = dist_img.shape[0]
+def distance_to_depth_conversion(dist_map, camera_fov=50.7):
+    img_width = dist_map.shape[1]
+    img_height = dist_map.shape[0]
     focal_in_pixels = (img_width * 0.5) / tan(camera_fov * 0.5 * pi / 180)
 
     # Get x_i and y_i (distances from optical center)
@@ -96,12 +113,14 @@ def distance_to_depth_conversion(dist_img, camera_fov=50.7):
     ys = np.arange(img_height) - cy
     xis, yis = np.meshgrid(xs, ys)
 
-    depth = np.sqrt(
-        dist_img ** 2 / (
+    depth_map = np.sqrt(
+        dist_map ** 2 / (
                 (xis ** 2 + yis ** 2) / (focal_in_pixels ** 2) + 1
         )
     )
-    return depth
+
+    return depth_map
+
 
 def process_obj(filepath):
     delete_scene_objects()
@@ -130,11 +149,17 @@ def process_obj(filepath):
 
     camera.rotation_euler = rot_quat.to_euler()
 
-    get_depthmap()
+    distance_map, rgb_image = get_distancemap_rgbmap()
+    depth_map = distance_to_depth_conversion(distance_map)
+    rgbd_image = np.concatenate([rgb_image, depth_map[..., None]], axis=-1)
+    print(rgbd_image)
+    bpy.context.scene.render.filepath = '/home/user/Documents/image.jpg'
+    bpy.ops.render.render(write_still=True)
 
 
 if __name__ == '__main__':
     file_loc = 'E:\\TFM\\Bakeoff 2020\\Job_0796_003477_MSLMST_obj_cart_single\\T000_P007_C000.obj'
+    # file_loc = 'E:\\TFM\\Bakeoff 2020\\Job_0796_003477_MSLMST_obj_sphr_single\\T000_P007_C000.obj'
     # load object config file
     print("Initialising ground plane fitter...")
     with open("C:\\Users\\migue\\PycharmProjects\\mars_dl\\environmentgenerator\\config.yaml") as f:
