87e46b57135073b90b5f5d34b7e4ec0a27cf78ce
Miguel Yuste Fernández <miguel_y_fernandez@hotmail.com>
2020-11-08 16:53:05 +0100

    [EnvGen] replaced plane fitting with PCA to align camera and object and implemented programmatic processing of OBJs

--- environmentgenerator/BlenderProcessor.py
+++ environmentgenerator/BlenderProcessor.py
@@ -1,13 +1,34 @@
-from yaml import load, FullLoader
-# import pywavefront
-from skspatial.objects import Points, Plane
-from skspatial.plotting import plot_3d
-import numpy as np
-import matplotlib.pyplot as plt
 import os
+from math import pi, tan
+from pathlib import Path
+
 import bpy
 import mathutils
-from math import pi, tan
+import numpy as np
+from sklearn.decomposition import PCA
+from skspatial.objects import Points, Plane
+from tqdm import tqdm
+from yaml import load, FullLoader
+
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
 
 def delete_scene_objects():
@@ -25,9 +46,6 @@ def delete_scene_objects():
             o.select_set(False)
     # bpy.data.objects['Camera'].select_set(False)
     bpy.ops.object.delete(True)
-
-    bpy.ops.wm.save_as_mainfile(filepath=bpy.data.filepath)
-    bpy.ops.wm.open_mainfile(filepath=bpy.data.filepath)
 
 
 def fit_plane(vertices):
@@ -68,7 +86,7 @@ def reverse_mapping(array, from_min=0, from_max=1, to_min=0.1, to_max=100):
 
 
 # distance map from camera to surfaces
-def get_distancemap_rgbmap():
+def get_distancemap_rgbimage():
     scene = bpy.data.scenes['Scene']
     tree = scene.node_tree
     # create output node
@@ -78,25 +96,23 @@ def get_distancemap_rgbmap():
     tree.nodes['Switch'].check = True
     bpy.ops.render.render(write_still=False)
     distance_map = np.asarray(bpy.data.images["Viewer Node"].pixels)
+
     # todo: if camera resolution can be tweaked, this can be parametrised
     distance_map = np.reshape(distance_map, (1080, 1920, 4))
     distance_map = distance_map[:, :, 0]
     distance_map = np.flipud(distance_map)
     distance_map = reverse_mapping(distance_map)
-    # print(distancemap)
 
     tree.nodes['Switch'].check = False
     bpy.ops.render.render(write_still=False)
     rgb_image = np.asarray(bpy.data.images["Viewer Node"].pixels)
+
     # todo: if camera resolution can be tweaked, this can be parametrised
     rgb_image = np.reshape(rgb_image, (1080, 1920, 4))
     rgb_image = rgb_image[:, :, 0:3]
     rgb_image = np.flipud(rgb_image)
     rgb_image = reverse_mapping(rgb_image)
-    # print(rgbmap.min())
-    # print(rgbmap.max())
 
-    # np.save(r"C:\Users\migue\Desktop/distance_from_camera.npy", depth)
     return distance_map, rgb_image
 
 
@@ -123,54 +139,69 @@ def distance_to_depth_conversion(dist_map, camera_fov=50.7):
 
 
 def process_obj(filepath):
-    delete_scene_objects()
+    # import object
     keys_before_import = bpy.data.objects.keys()
     bpy.ops.import_scene.obj(filepath=filepath)
+    # todo: is this necessary?
     imported_keys = [s for s in bpy.data.objects.keys() if s not in keys_before_import]
-    print(keys_before_import)
-    # object = bpy.context.selected_objects[0] ####<--Fix
-    object = bpy.data.objects[imported_keys[0]]  ####<--Fix
-    object.rotation_euler = mathutils.Vector([0, 0, 0])  # clear any possible rotation set in the obj
-    print('Imported name: ', object.name)
-    # print(object.data.vertices.values())
+    # select object
+    object = bpy.data.objects[imported_keys[0]]
+    # clear any possible rotation set in the obj
+    object.rotation_euler = mathutils.Vector([0, 0, 0])
+    print('Processing obj: ', object.name)
+    # extract vertices
     vertices = np.empty([len(bpy.data.meshes[object.name].vertices), 3], dtype=np.float32)
     for i, vertex in enumerate(bpy.data.meshes[object.name].vertices):
         vertices[i] = np.asarray(vertex.co)
+    # calculate center point and PCA for camera placing
     estimated_center_point = vertices.mean(axis=0)
-
-    # print(np.asarray(bpy.data.meshes[object.name].vertices[0].co))
-    # ToDo: is point centered? otherwise, average all vertices
-    plane = fit_plane(vertices)
+    pca = PCA(n_components=3)
+    pca.fit(vertices)
+    V = pca.components_
+    width_comp = V.T[0]
+    normal_comp = V.T[2]
     camera = bpy.data.objects['Camera']
-    # todo: greater camera distance
-    camera.location = estimated_center_point + 5 * np.asarray(plane.normal)
+    # point camera at object central point
+    camera.location = estimated_center_point + np.asarray(normal_comp)
+    # rotate and align camera with object
     looking_direction = camera.location - mathutils.Vector(estimated_center_point)
     rot_quat = looking_direction.to_track_quat('Z', 'Y')
-
-    camera.rotation_euler = rot_quat.to_euler()
-
-    distance_map, rgb_image = get_distancemap_rgbmap()
+    cam_x = rot_quat @ mathutils.Vector((1.0, 0.0, 0.0))
+    roll = angle_between(width_comp, np.asarray(cam_x))
+    camera_roll = mathutils.Matrix.Rotation(roll, 4, 'Z').to_quaternion()
+    camera.rotation_euler = (rot_quat @ camera_roll).to_euler()
+    # close up on object (fit entire object into view)
+    bpy.ops.view3d.camera_to_view_selected()
+    # get distance to camera and RGB values
+    distance_map, rgb_image = get_distancemap_rgbimage()
+    # delete object so it isn't visible in the next image
+    bpy.ops.object.select_all(action='DESELECT')
+    object.select_set(True)
+    bpy.ops.object.delete()
+    # convert camera distances to actual depth data
     depth_map = distance_to_depth_conversion(distance_map)
+    # prepare and save as numpy array
     rgbd_image = np.concatenate([rgb_image, depth_map[..., None]], axis=-1)
-    print(rgbd_image)
-    bpy.context.scene.render.filepath = '/home/user/Documents/image.jpg'
-    bpy.ops.render.render(write_still=True)
+    np.save(str(path_out + Path(filepath).stem + ".npy"), rgbd_image)
 
 
 if __name__ == '__main__':
     file_loc = 'E:\\TFM\\Bakeoff 2020\\Job_0796_003477_MSLMST_obj_cart_single\\T000_P007_C000.obj'
-    # file_loc = 'E:\\TFM\\Bakeoff 2020\\Job_0796_003477_MSLMST_obj_sphr_single\\T000_P007_C000.obj'
     # load object config file
-    print("Initialising ground plane fitter...")
+    print("Initialising Blender processor...")
     with open("C:\\Users\\migue\\PycharmProjects\\mars_dl\\environmentgenerator\\config.yaml") as f:
         config = load(f, Loader=FullLoader)
-    path_in = config['obj2heightmap']['path_in']
-    obj_in = config['obj2heightmap']['obj_in']
-
+    path_in = config['blenderprocessor']['path_in']
+    path_out = config['blenderprocessor']['path_out']
+    # create output path if it doesn't exist
+    if not os.path.exists(path_out):
+        os.makedirs(path_out)
     # load scene
     scene = bpy.data.scenes['Scene']
     # make scene the active one
     bpy.context.window.scene = scene
-    process_obj(file_loc)
-
-
+    # delete unecessary objects
+    delete_scene_objects()
+    print("Commencing data extraction...")
+    for file in tqdm(Path(path_in).rglob('*.obj'), desc='Processing OBJs:'):
+        process_obj(str(file))
