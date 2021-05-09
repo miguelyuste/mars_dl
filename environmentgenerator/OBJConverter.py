[EnvGen] Fixed outlier detection and smoothing, reworked 3D coordinates calculation avoiding matmul, added parallelisation

environmentgenerator/OBJConverter.py
@@ -9,7 +9,7 @@ from PIL import Image
 from pathlib import Path
 from math import pi, tan
 from scipy.ndimage.filters import gaussian_filter, median_filter
-from scipy.spatial import Delaunay
+#from scipy.spatial import Delaunay
 import sys
 
 # focal length from camera intrinsic matrix (obtained from Blender)
@@ -45,52 +45,52 @@ def to_obj(tile, outfile):
 
     rgb = (tile[...,:3] / 2) + 0.5 # tile has values in [-1,1], map RGB to [0,1]
     z = tile[:, :, 3]
-    #
-
-    if is_image:
-        z = undo_conversion(z)
-
-    # Flatten z and project depth values to their 3D coordinates
-
-    idx = np.mgrid[:z.shape[0], :z.shape[1]].reshape([2, -1])
-    z = z.reshape([1, -1])
+    z = median_filter(z, size=10)
+    z = gaussian_filter(z, sigma=0.5)
     # Todo:interpolation necessary?
     #z = np.interp(z, (z.min(), z.max()), (+0.5, +1))
-    z = median_filter(z, size=3)
-    V = np.vstack([idx, np.ones_like(z), 1 / z])
+    #
 
-    # todo: parametrise f_x and f_y
-    M = np.eye(4)
-    M[0, 0] = 0.000375
-    M[1, 1] = 0.000375
-
-    # 3D coordinates of points (flattened)
-    C = (z * (M @ V)).reshape([-1, z.shape[0], z.shape[1]])
-
-    # Undo flattening (TODO: Check that the reshaping is in the correct order)
-    C = C.squeeze().reshape([4, *tile.shape[:2]])
-
-    # Calculate variables for outlier filtering
+    ### OUTLIER FILTERING
+    # Calculate outlier filtering parameters
     mu = np.mean(z)
     # If height values are mostly negative, invert sign
-    if(mu<0):
+    if (mu < 0):
         z = -z
         mu = -mu
     sigma = np.std(z)
-    inliers_idx = np.abs(C[3,...] - mu) < 2 * sigma
-    ######## TODO: uncomment this
-    #inliers_idx = np.ones(C[3,...].shape,np.bool)
-    TOTAL_POINTS = (tile.shape[0]*tile.shape[1])
-    num_outliers = TOTAL_POINTS-inliers_idx.sum()
-    MAX_OUTLIERS = 0.05*TOTAL_POINTS
+    ######## TODO: uncomment this (or not - this sets all values to one!)
+    inliers_idx = np.abs(z - mu) < 1.5*sigma
+    #inliers_idx = np.ones(z.shape, np.bool)
+    TOTAL_POINTS = (tile.shape[0] * tile.shape[1])
+    num_outliers = TOTAL_POINTS - inliers_idx.sum()
+    MAX_OUTLIERS = 0.05 * TOTAL_POINTS
     # If there are many outliers, but within a reasonable treshold, keep tile and don't filter outliers
-    if MAX_OUTLIERS <= num_outliers <= 2*MAX_OUTLIERS:
-        print(f'Skipping outliers filtering because there are {num_outliers} outliers (mu={mu}, std={sigma}, max outliers={MAX_OUTLIERS}).')
+    #if MAX_OUTLIERS <= num_outliers <= 2 * MAX_OUTLIERS:
+    #    print(
+    #        f'Skipping outliers filtering because there are {num_outliers} outliers (mu={mu}, std={sigma}, max outliers={MAX_OUTLIERS}).')
     # If number of outliers > 2*max_outliers: discard tile
     # elif num_outliers > 2*MAX_OUTLIERS:
     #     print(f'Tile (mu={mu}, std={sigma}) contains {num_outliers} outliers, which is more than two times the treshold of max outliers({MAX_OUTLIERS}). Discarding tile.')
     #     return
 
+    if is_image:
+        z = undo_conversion(z)
+
+    # Flatten z and project depth values to their 3D coordinates
+    # two subarrays for x and y coord of length 1024x1024
+    idx = np.mgrid[:z.shape[0], :z.shape[1]].reshape([2, -1])
+    z = z.reshape([1, -1])
+
+    # todo: parametrise f_x and f_y
+    # Directly calculate point coordinates without passing through matrix multiplication (this is more accurate)
+    C_alt = np.ones([4, idx.shape[1]])
+    C_alt[0, :] = z[0, :] * 1/f_x * idx[0, :]
+    C_alt[1, :] = z[0, :] * 1/f_y * idx[1, :]
+    C_alt[2, :] = z[0, :]
+
+    # Reshape back to 2D image
+    C = C_alt.reshape([4, *tile.shape[:2]])
 
     # Todo: are really all cases so extreme?
     # Build vertex index-coordinates map for later triangulation
@@ -230,8 +230,14 @@ if __name__ == '__main__':
 
     # process texture mosaics
     #to_process = [file for file in glob(path_in + "**/*.npy")]
-    to_process = [file.relative_to(path_in) for file in path_in.glob("*/*.npy")]
-    for mosaic_path in tqdm(to_process, desc="Converting SGAN results into OBJ format"):
-        process_mosaic(mosaic_path)
+    to_process = [file.relative_to(path_in) for file in path_in.glob("**/*.npy")]
+    #for mosaic_path in tqdm(to_process, desc="Converting SGAN results into OBJ format"):
+    #    process_mosaic(mosaic_path)
+
+    try:
+        Parallel(n_jobs=-1, backend="loky")(
+            map(delayed(process_mosaic), (mosaic_path for mosaic_path in tqdm(to_process, desc="Converting SGAN results into OBJ format"))))
+    except Exception as e:
+        print("Exception while concurrently processing SGAN results: " + repr(e))
 
     print("Dataset successfully processed.")