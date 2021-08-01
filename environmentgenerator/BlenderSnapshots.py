dd52d8c64709cf83ad2d5a0c1e6a810217ed04d2
Miguel Yuste Fernández <miguel_y_fernandez@hotmail.com>
2021-08-02 00:30:38 +0200

    [EnvGen] First functional version of BlenderSnapshots.py

--- environmentgenerator/BlenderSnapshots.py
+++ environmentgenerator/BlenderSnapshots.py
@@ -5,28 +5,55 @@ from tqdm import tqdm
 import logging
 import datetime
 import random
+import mathutils
+import numpy as np
+from math import radians
+
 
 def generate_point():
     x = random.uniform(coords['lower_x'], coords['upper_x'])
     y = random.uniform(coords['lower_y'], coords['upper_y'])
     z = random.uniform(coords['lower_z'], coords['upper_z'])
-    return [x,y,z]
+    return mathutils.Vector((x, y, z))
 
-def process_obj(object_key):
-    # if object isn't a light source, terrain nor camera
-    logger.info("Processing OBJ: " + object_key)
-    if "light" not in object_key.lower() and "generated" not in object_key.lower() and "camera" not in object_key.lower():
-        # select object
-        logger.info("      Selecting object")
-        obj = bpy.data.objects[object_key]
-        # center camera on object
-        logger.info("      Centering camera on object")
-        bpy.ops.view3d.camera_to_view_selected()
-        logger.info("      Rendering object")
-        bpy.context.scene.render.filepath = os.path.join(path_out, object_key)
-        bpy.ops.render.render(write_still=False)
-    else:
-        logger.info("      OBJ isn't a shatter cone, skipping it")
+
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
+def take_snapshot(iteration):
+    logger.info("      Centering camera on object")
+    # get random points
+    point_a = generate_point()
+    point_b = generate_point()
+    # select camera
+    camera = bpy.data.objects['Camera']
+    # first point is camera location, difference between both is forward vector
+    camera.location = point_a
+    forward_vector = point_a - point_b
+    # rotate camera along forward vector
+    rot_quat = forward_vector.to_track_quat('Z', 'Y')
+    camera.rotation_euler = (rot_quat.to_matrix().to_4x4() @ mathutils.Matrix.Rotation(radians(90.0), 4, 'Z')).to_euler()
+    # render and save view
+    logger.info("      Rendering view")
+    bpy.context.scene.render.filepath = os.path.join(path_out, str(iteration))
+    bpy.ops.render.render(write_still=True)
 
 
 if __name__ == '__main__':
@@ -37,6 +64,7 @@ if __name__ == '__main__':
         config = load(f, Loader=FullLoader)
     path_out = config['blendersnapshots']['path_out'] + datetime.datetime.now().strftime("%Y-%m-%d %Hh%Mm%Ss")
     coords = config['blendersnapshots']['coordinates']
+    no_snaps = config['blendersnapshots']['no_snaps']
 
     # create output path if it doesn't exist
     if not os.path.exists(path_out):
@@ -65,29 +93,13 @@ if __name__ == '__main__':
     bpy.context.window.scene = scene
     # delete unecessary objects
     print("Commencing data extraction...")
-
-    # # add lighting
-    # logger.info("Adding lighting")
-    # # create light datablock, set attributes
-    # light_data = bpy.data.lights.new(name="light_2.80", type='POINT')
-    # light_data.energy = 100
-
-    # create new object with our light datablock
-    # light_object = bpy.data.objects.new(name="light_2.80", object_data=light_data)
-
-    # link light object
-    # bpy.context.collection.objects.link(light_object)
-
-    # make it active
-    # bpy.context.view_layer.objects.active = light_object
-
-    # launch OBJ processing
+    # launch snapshot processing
     errors = 0
-    for object_key in tqdm(bpy.data.objects.keys(), desc='Processing shatter cones:'):
+    for i in tqdm(range(no_snaps), desc='Processing snapshots:'):
         try:
-            process_obj(object_key)
+            take_snapshot(i)
         except Exception:
-            error = error + 1
+            errors = errors + 1
             logger.error("Fatal error in OBJ processing", exc_info=True)
     if errors == 0:
         print("All input OBJs have been successfully processed. See the logs for more details.")
